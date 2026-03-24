4fcab225ce33e869f0951434e9f0d39fdac48548
Davy Jean-Charles <dave.jc971@gmail.com>
2026-03-24 18:32:02 -0400

    Add react app with vite then start creating the schema

--- frontend/src/pages/home.js
+++
@@ -1,9 +0,0 @@
-const Home = () => {
-  return (
-    <div className="container">
-      <h1>Bienvenue sur notre plateforme</h1>
-      <p>Contenu de l'accueil bientôt ici...</p>
-    </div>
-  );
-};
-export default Home;
